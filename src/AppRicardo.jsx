e597bccd90b31198c2c1edc5f09dfa7d17d10b63
hllricardo <hernandezlloretricardo@.gmail.com>
2025-11-06 12:18:05 +0100

    Ya muestra los diferentes projectos que tenemos en data, faltan algunos datos en los projectos para que se parezca mas a la idea que mostre, tambien faltan los estilos para darle forma

---
+++ src/AppRicardo.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ProjectList from './components/ProjectList'
+
+function AppRicardo() {
+
+  return (
+    <>
+    <ProjectList />
+    </>
+  )
+}
+
+export default AppRicardo
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import AppRicardo from './AppRicardo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppRicardo />
   </StrictMode>,
 )
