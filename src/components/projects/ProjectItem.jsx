58562ce0e30fcf2d04cab75b6f07c4ddeb44b00d
hllricardo <hernandezlloretricardo@.gmail.com>
2025-11-07 19:34:06 +0100

    Estilos basicos de estructura para projectos

--- src/components/projects/ProjectItem.jsx
+++ src/components/projects/ProjectItem.jsx
@@ -2,23 +2,20 @@
 function ProjectItem({ id, name, client, status, description, tasks }) {
 
     const taskItems = [];
-  for (let i = 0; i < tasks.length; i++) {
-    taskItems.push(<li key={i}>{tasks[i]}</li>);
-  }
+    for (let i = 0; i < tasks.length; i++) {
+        taskItems.push(<li key={i}>{tasks[i]}</li>);
+    }
 
     return (
         <>
             <div key={id} className="box-project">
                 <div>
-                    <h3>{name}</h3>
-                    <p>{client}</p>
-                    <p>{description}</p>
-                    <div>
-                        <ul>
-                            {taskItems}
-                        </ul>
-                            
-                    </div>
+                    <h3 className="name-project">{name}</h3>
+                    <p className="client-project">{client}</p>
+                    <p className="description-project">{description}</p>
+                    <ul className="box-list-tasks">
+                        {taskItems}
+                    </ul>
                 </div>
                 <div>
                     <p className="status-project" >{status}</p>
