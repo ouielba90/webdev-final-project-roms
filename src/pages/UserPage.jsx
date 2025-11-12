d4b07a35985889936eecfea0195093865a50b0de
ouielba90 <oui.elba.1990@gmail.com>
2025-11-12 20:32:28 +0100

    fix a typo on UserPage.jsx

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,24 +1,24 @@
 import { Link, Outlet } from "react-router-dom"
-import {users} from "./../../data/users.js"
-import UserCard from "./users/userCard.jsx"
+import { users } from "./../../data/users.js"
+import UserCard from "./users/UserCard.jsx"
 
 function UserPage() {
-    console.log(users)
-    return (
-        <>
-            <div>
-                <h1>Lista de usuarios</h1>
-            <div className="usersContainer">
-                {users.map((user) => {
-                    return (
-                        <UserCard user={user}/>
-                    )
-                })
-            }
-            </div>
-            </div>
-        </>
-    )
+  console.log(users)
+  return (
+    <>
+      <div>
+        <h1>Lista de usuarios</h1>
+        <div className="usersContainer">
+          {users.map((user) => {
+            return (
+              <UserCard user={user} />
+            )
+          })
+          }
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default UserPage
