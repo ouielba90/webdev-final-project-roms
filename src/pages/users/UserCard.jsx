82c16e68d5cc81a8105b8a91112d2da5cb290f6c
MarcCano <marccanoarceus@gmail.com>
2025-11-22 13:42:15 +0100

    formulario de edición del usuario hecho y funcionando

--- src/pages/users/UserCard.jsx
+++ src/pages/users/UserCard.jsx
@@ -1,48 +1,58 @@
-function UserCard({ user, onDeleteUser }) {
-    function userStatus(estado) {
-    if(estado === 'activo') {
-        return ({
-            background: '#12a912ff',
-        })
-    }
+function UserCard({ user, onDeleteUser, onEditUser }) {
+	function userStatus(estado) {
+		if (estado === 'activo') {
+			return ({
+				background: '#12a912ff',
+			})
+		}
 
-    if(estado === 'inactivo') {
-        return ({
-            background: '#bfbfbfff',
-        })
-    }
+		if (estado === 'inactivo') {
+			return ({
+				background: '#bfbfbfff',
+			})
+		}
 
-    if(estado === 'ausente') {
-        return ({
-            background: '#ffbf00',
-        })
-    }
+		if (estado === 'ausente') {
+			return ({
+				background: '#ffbf00',
+			})
+		}
 
-    if(estado === 'ocupado') {
-        return ({
-            background: '#dc143c',
-        })
-    }
-}
-    return (
-        <div className="userDiv">
-            <div className="user-data">
-                <p>
-                    {user.name} <br />
-                    {user.email} <br />
-                    {user.role}
-                </p>
-            </div>
+		if (estado === 'ocupado') {
+			return ({
+				background: '#dc143c',
+			})
+		}
+	}
+	return (
+		<div className="userDiv">
+			<div className="user-data">
+				<p>
+					{user.name} <br />
+					{user.email} <br />
+					{user.role}
+				</p>
+			</div>
 
-            <div>
-                <div className="estado" style={userStatus(user.status)}></div>
-                <h1 className="estado-txt">{user.status}</h1>
-            </div>
-            <div>
-                <button onClick={() => onDeleteUser(user.id)} className="del-btn">Eliminar</button>
-            </div>
-        </div>
-    )
+			<div>
+				<div className="estado" style={userStatus(user.status)}></div>
+				<h1 className="estado-txt">{user.status}</h1>
+			</div>
+
+			<div className="btn-box">
+				<button
+					onClick={onDeleteUser}
+					className="user-btn">Eliminar
+				</button>
+
+				<button
+					onClick={onEditUser}
+					className="user-btn">
+					Editar
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default UserCard
